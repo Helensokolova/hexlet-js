67cf08d2cb213fd7e113abc868e62da8e0bfbd21
Elena Sokolova <asn-t@yandex.ru>
2021-01-15 09:43:16 +0300

    eslint prettier

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello, Hexlet");
-import fs from "fs";
-import _ from "lodash";
-console.log(_.last(['one', 'two']));
---
+++ src/app.js
@@ -0,0 +1,8 @@
+var a=  1
+function foo (  name) {
+    var lastname=name
+}
+
+var x =200
+
+console.log(x)
